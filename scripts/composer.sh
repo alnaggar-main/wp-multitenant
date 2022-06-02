524fef10c4eba794a623406dd32ecc46c77965de
olfast <olfast.com@gmail.com>
2022-06-02 14:17:10 +0300

    d

--- scripts/composer.sh
+++ scripts/composer.sh
@@ -3,9 +3,6 @@ cp _install-files/_wp-bedrock.php assets/mu-plugins/bedrock-autoloader.php
 cp _install-files/_wp-silencer.php assets/plugins/index.php
 cp _install-files/_wp-silencer.php assets/themes/index.php
 
-## Redis Object Cache
-cp assets/mu-plugins/litespeed-cache/lib/object-cache.php assets/drop-ins
-
 
 ## Change imptrnat file permssion 
 sudo chmod 400 app/wp-config.php
