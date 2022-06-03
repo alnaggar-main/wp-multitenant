dc96be64e7e615cbb30b24c19681d034aae977fc
olfast <olfast.com@gmail.com>
2022-06-03 03:05:40 +0300

    Update composer.sh

--- scripts/composer.sh
+++ scripts/composer.sh
@@ -2,9 +2,9 @@
 sudo mkdir assets/mu-plugins
 
 ## Bedrock Autoloader for mu-plugins
-cp _install-files/_wp-bedrock.php assets/mu-plugins/bedrock-autoloader.php
-cp _install-files/_wp-silencer.php assets/plugins/index.php
-cp _install-files/_wp-silencer.php assets/themes/index.php
+sudo cp _install-files/_wp-bedrock.php assets/mu-plugins/bedrock-autoloader.php
+sudo cp _install-files/_wp-silencer.php assets/plugins/index.php
+sudo cp _install-files/_wp-silencer.php assets/themes/index.php
 
 
 ## Change imptrnat file permssion 
